ed7937e4712f3304966f16b36bd6d643815e4c57
amandalmeidachagas <amandaalmeidachagas@gmail.com>
2022-05-15 14:32:21 -0300

    Exercício de variáveis

---
+++ fundamentos/bloco-04-js/dia-01-primeiros-passos/scripts.js
@@ -0,0 +1,14 @@
+const myName = "Amanda";
+const birthCity = "Belo Horizonte";
+let birthYear = 1992;
+
+console.log (myName);
+console.log (birthCity);
+console.log (birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "São Paulo";
+console.log(birthCity);
+// A questão número 6 da um erro pois birthCity é uma constante e não pode ser alterada
